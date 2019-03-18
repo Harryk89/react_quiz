e0d20a0ec90f35afba80dffaa9ce8f0e19fb7135
Harryk89 <sapsayartyom@gmail.com>
2019-03-19 01:11:32 +0200

    Updates

---
+++ precache-manifest.f287acba0c4e13f830a75c5805951707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e90f3a33fac45c0c36c2",
+    "url": "/react_quiz/static/js/runtime~main.e90f3a33.js"
+  },
+  {
+    "revision": "56280fc73a501eed60ac",
+    "url": "/react_quiz/static/js/main.56280fc7.chunk.js"
+  },
+  {
+    "revision": "e3b9b27be7749a750286",
+    "url": "/react_quiz/static/js/1.e3b9b27b.chunk.js"
+  },
+  {
+    "revision": "56280fc73a501eed60ac",
+    "url": "/react_quiz/static/css/main.14b94d58.chunk.css"
+  },
+  {
+    "revision": "d10d12c30510de549f49e11f9817c878",
+    "url": "/react_quiz/index.html"
+  }
+];
